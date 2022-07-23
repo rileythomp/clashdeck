4025180f723d66bcd78e04ede84be4839044bc67
Riley Thompson <rileythompson99@gmail.com>
2022-07-23 01:48:35 -0400

    add heroku url

--- src/app/clash-api.service.ts
+++ src/app/clash-api.service.ts
@@ -7,13 +7,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ClashApiService {
 
-	readonly API_URL = 'http://localhost:5000';
+	// readonly API_URL = 'http://localhost:5000/api/v1/deck';
+	readonly API_URL = 'https://clashdeck.herokuapp.com/api/v1/deck'
 
 	constructor(private http: HttpClient) { }
 
 	getDeck(player: string, trophies: string): Observable<any> {
 		return this.http.get<any>(
-			`${this.API_URL}/api/v1/deck?player=${player}&trophies=${trophies}`,
+			`${this.API_URL}?player=${player}&trophies=${trophies}`,
 			{
 				headers: new HttpHeaders({
 					'Content-Type': 'application/json',
