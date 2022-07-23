24e529d706ecfa37bc128f7c4800969cdcb9aedf
Riley Thompson <rileythompson99@gmail.com>
2022-07-23 04:04:18 -0400

    use s3

--- backend/clashdeck.py
+++ backend/clashdeck.py
@@ -1,10 +1,14 @@
+from os import getenv
 import requests
 from bs4 import BeautifulSoup
 import sys
 from time import time
 
+API_URL = getenv('API_URL')
+S3_ADDR = getenv('S3_ADDR')
+
 def get_deck(player: str, trophies: int):
-    url = f'https://royaleapi.com/player/search/results?q={player}'
+    url = f'{API_URL}/search/results?q={player}'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
     }
@@ -13,7 +17,7 @@ def get_deck(player: str, trophies: int):
     players = soup.find_all('tr')
     for player in players:
         player_tag = player.find(class_='player_tag').text.strip('#')
-        deck_url = f'https://royaleapi.com/player/{player_tag}/decks'
+        deck_url = f'{API_URL}/{player_tag}/decks'
         deck_page = requests.get(deck_url, headers=headers)
         deck_soup = BeautifulSoup(deck_page.content, 'html.parser')
         try:
@@ -26,7 +30,7 @@ def get_deck(player: str, trophies: int):
             deck_cards = deck_soup.find_all(class_='deck_card')[:8]
             deck = []
             for card in deck_cards:
-                card_url = card['src']
+                card_url = f"{S3_ADDR}/{card['src'].split('/')[-1].split('?')[0]}"
                 card_name = card_url.split('/')[-1].split('.')[0].replace('-', ' ').title()
                 deck.append({'card_name': card_name, 'card_url': card_url})
             return deck
